518736ec3aa6d8d542b8b676675fe67f1a4e92f5
Bhavesh <bhavesh101714@gmail.com>
2025-09-25 00:53:44 +0530

    11_Functions file added

---
+++ 02_sum.py
@@ -0,0 +1,5 @@
+def sum(a, b):
+    return a + b
+c = sum(10,10)
+print(c)
+print(sum(35,100))
---
+++ 06_keyword_argument.py
@@ -0,0 +1,3 @@
+def details(name, age):
+    return f"Name : {name}, Age : {age}"
+print(details(age = 24, name = "Bhavesh"))
